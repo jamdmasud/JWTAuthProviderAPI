81d3fa544ec1eee61d01435c8c579941ff4f148e
Md. Masud <jamdmasud@gmail.com>
2018-01-03 22:35:33 +0600

    Add project files.

---
+++ JWTAuthApp/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace JWTAuthApp.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
